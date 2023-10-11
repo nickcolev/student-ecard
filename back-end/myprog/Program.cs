31c0fc35d8373f0e9c96942551816076ffbfbff6
Nick Kolev <nickcolev@gmail.com>
2023-10-11 16:44:45 +0300

    Added back-end -- C# code for CGI (VS project)

---
+++ back-end/myprog/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myprog
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+#if DEBUG
+            Environment.SetEnvironmentVariable("QUERY_STRING", "3");
+#endif
+            string qs = Environment.GetEnvironmentVariable("QUERY_STRING");
+            if (qs == null)
+            {
+                err_response("usage: prog number");
+            }
+            else
+            {
+                try
+                {
+                    int r = int.Parse(qs);
+                    string line = GetRowFromTextFile("student.csv", r);
+                    // Изпращаме резултат на клиента (Front-End)
+                    if( line == null )
+                        err_response("out of range");
+                    else
+                        Console.Write("Content-Type: text/plain\n\n" + line);
+                }
+                catch (Exception e) { err_response("bad argument"); }
+            }
+        }
+        // Обща функция за връщане на грешка на клиента
+        public static void err_response(string msg)
+        {
+            // CGI изисква да се изпрати Mime-Type
+            Console.Write("Content-Type: text/plain\n\n" + msg);
+            Environment.Exit(1);
+        }
+        public static string GetRowFromTextFile(string fileName, int rowNumber)
+        {
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                for (int i = 1; i < rowNumber; i++)
+                {
+                    reader.ReadLine();
+                }
+
+                return reader.ReadLine();
+            }
+        }
+
+    }
+}
